ca146016f6d00d8c164aa7d983722bc1ff1422c4
wenjing-liu <wenjing.liu09@foxmail.com>
2018-04-27 16:44:32 +0800

    Maps, hashMaps

--- script.js
+++ script.js
@@ -352,3 +352,41 @@ function SmithFunction(firstName, yearOfBirth, lastName = 'Smith', nationality =
 }
 
 
+// Maps
+const question = new Map()
+question.set('question', 'What is the offical name of the lastest major JS version?')
+question.set(1, 'ES5')
+question.set(2, 'ES6')
+question.set(3, 'ES2015')
+question.set(4, 'ES7')
+question.set('correct', 3)
+question.set(true, 'Correct answer:D')
+question.set(false, 'Wrong, please try again')
+
+console.log(question.get('question'))
+console.log(question.size)
+if (question.has(4)) {
+    // question.delete(4)
+    console.log(question.get(4))
+}
+
+// question.clear()
+
+question.forEach((value, key) => console.log(`This is ${key}, and it is set to ${value}`))
+
+for (let [key, value] of question.entries()) {
+    if (typeof(key) === 'number') {
+        console.log(`Answer ${key}: ${value}`)
+    }
+    console.log(`This is ${key}, and it is set to ${value}`)
+}
+
+const ans = parseInt(prompt('Write the correct answer'))
+
+
+console.log(question.get(ans = question.get('correct')))
+
+
+
+
+
