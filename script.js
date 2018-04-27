94cd088d80a401e3b6757a8d32a6e3ef09f85335
wenjing-liu <wenjing.liu09@foxmail.com>
2018-04-27 11:31:30 +0800

    blocks vs IIFEs

--- script.js
+++ script.js
@@ -52,3 +52,25 @@ for (let i = 0; i < 5; i++) {
 }
 
 console.log(i) // 23
+
+
+//***********************************************//
+// Blocks and IIFEs
+
+// ES6
+{
+    const a = 1
+    let b = 2
+    var c = 3
+}
+
+console.log(a + b)  // error: not defined
+console.log(c)
+
+// ES5
+(function () {
+    var c = 3
+})()
+
+console.log(c)
+
