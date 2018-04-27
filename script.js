984abf7595df80a3f34fb74435392584a97a4f09
wenjing-liu <wenjing.liu09@foxmail.com>
2018-04-27 14:50:57 +0800

    .from, .find, .findIndex method of Array

--- script.js
+++ script.js
@@ -227,3 +227,54 @@ const [age, retirement] = calcAgeRetirement(1990)
 console.log(age)
 console.log(retirement)
 
+
+// Arrays
+
+const boxes = document.querySelectorAll('.box')
+
+// ES5
+var boxesArr5 = Array.prototype.slice.call(boxes)
+boxesArr5.forEach(function(curr){
+    cur.style.backgroundColor = 'dodgerblue'
+});
+
+
+// ES6
+const boxes6 = Array.from(boxes)
+boxes6.forEach(curr => {
+    cur.style.backgroundColor = 'dodgerblue'
+})
+
+// ES5
+for (var i = 0; i < boxesArr5.length; i++){
+    if (boxesArr5[i].className === 'box blue') {
+        continue
+    }
+    boxesArr5.textContent = 'I changed to blue'
+}
+
+// ES6
+
+for (const cur of boxes6) {
+    if (cur.className.includes(blue)) {
+        continue
+    }
+    cur.textContent = 'I changed to blue'
+}
+
+
+// ES5
+var ages = [12, 17, 8, 21, 14, 11]
+var full = ages.map(function (cur) {
+    return cur >= 18
+})
+
+console.log(full)
+
+console.log(full.indexOf(true))
+
+console.log(ages[full.indexOf(true)])
+
+// ES6
+console.log(ages.findIndex(cur => cur >= 18))
+console.log(ages.find(cur => cur > 18))
