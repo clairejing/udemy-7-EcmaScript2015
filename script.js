78e2108c6774691a515b62bdc89e1d76e19ed6ec
wenjing-liu <wenjing.liu09@foxmail.com>
2018-04-27 15:06:07 +0800

    spread operator

--- script.js
+++ script.js
@@ -278,3 +278,35 @@ console.log(ages[full.indexOf(true)])
 // ES6
 console.log(ages.findIndex(cur => cur >= 18))
 console.log(ages.find(cur => cur > 18))
+
+
+//Spread Operator
+
+function addFourAges (a, b, c, d) {
+    return a + b + c + d
+}
+
+var sum1 = addFourAges(18, 30, 12, 21)
+console.log(sum1)
+
+// ES5
+var ages = [18, 30, 12, 21]
+
+var sum2 = addFourAges.apply(null, ages)
+console.log(sum2)
+// ES6 
+var sum3 = addFourAges(...ages)
+console.log(sum3) 
+
+const familySmith = ['John', 'Jane', 'Mark']
+const familyMark = ['Mary', 'Bob']
+const bigFamily = [...familySmith, ...familyMark, 'Lily']
+console.log(bigFamily)
+
+
+const h = document.querySelector('h1')
+const boxes = document.querySelectorAll('.box')
+const all = [h, ...boxes]
+Array.from(all).forEach(cur => cur.style.color = 'purple')
+
+
