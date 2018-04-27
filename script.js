612636aeff0049a929fe30f65fbe0a10180b44c9
wenjing-liu <wenjing.liu09@foxmail.com>
2018-04-27 11:46:41 +0800

    template literal

--- script.js
+++ script.js
@@ -74,3 +74,30 @@ console.log(c)
 
 console.log(c)
 
+
+
+
+//***********************************************//
+// Strings
+
+
+let firstName = 'john'
+let lastName = 'Smith'
+const yearOfBirth = 1990
+
+function calcAge(year) {
+    return 2016 - year
+}
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is' + calcAge(yearOfBirth) + 'years old.')
+
+// ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old. `)
+
+
+const n = `${firstName} ${lastName}`
+n.startsWith('J')
+n.endsWith('Sm')
+n.includes('oh')
+`${firstName} `.repeat(5)
